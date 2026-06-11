3649abbc747cf5285a030d1de3340a3621832ec9
Vineet Anand <vineetanandvicky@gmail.com>
2026-06-11 20:38:24 +0530

    sortAllValuesInDescOrder

---
+++ sortAllValuesInDescOrder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.lang.*;
+import java.io.*;
+
+class Test
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// sort all the values present in the given Integer list in 
+		// descending order 
+		Arrays.asList(1, 2, 3, 4, 8, 3, 0, 20).stream()
+		.sorted(Comparator.reverseOrder())
+		.forEach(System.out :: println);
+		
+	}
+}
