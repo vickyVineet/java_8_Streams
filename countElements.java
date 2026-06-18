ed42563441a4f0ec34caad4527aad4832e24ba0d
Vineet Anand <vineetanandvicky@gmail.com>
2026-06-18 19:55:56 +0530

    countElements

---
+++ countElements.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Test{
+	public static void main (String[] args) throws java.lang.Exception{
+		// count elements
+		System.out.println("Count:- " + Arrays.asList(1, 3, 2, 7, 4).stream()
+		.count());
+
+	}
+}
